8b5a3b29305be71cf764e39c19889c5238bff8d2
Andrej Karadzic <karadza3a@yahoo.com>
2015-11-24 02:13:59 +0100

    Initial physics

---
+++ Assets/Kicking.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kicking : MonoBehaviour
+{
+	public float power = 20f;
+
+	private CircleCollider2D colider;
+	private CircleCollider2D ballColider;
+	private Rigidbody2D ballBody;
+	private int lastKick = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		colider = gameObject.GetComponent<CircleCollider2D> ();
+		GameObject ball = GameObject.FindGameObjectsWithTag ("Ball") [0];
+		ballColider = ball.GetComponent<CircleCollider2D> ();
+		ballBody = ball.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey ("x") 
+			&& lastKick <= Time.frameCount - 3
+			&& colider.IsTouching (ballColider)) {
+
+			Vector2 v = ballBody.gameObject.transform.position - transform.position;
+			v = v / v.magnitude;
+			Debug.Log (v.magnitude);
+			ballBody.AddForce (power * v);
+			lastKick = Time.frameCount;
+		}
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float playerSpeed;
+	public float maxSpeed;
+
+	private Rigidbody2D rigidb;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigidb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		MoveForward ();
+		rigidb.velocity = Vector2.ClampMagnitude (rigidb.velocity, maxSpeed);
+	}
+	
+	void MoveForward ()
+	{	
+		if (Input.GetKey ("up")) {//Press up arrow key to move forward on the Y AXIS
+			rigidb.AddForce (new Vector2 (0, playerSpeed * Time.deltaTime));
+		}
+		if (Input.GetKey ("down")) {//Press up arrow key to move forward on the Y AXIS
+			rigidb.AddForce (new Vector2 (0, -playerSpeed * Time.deltaTime));
+		}
+		if (Input.GetKey ("left")) {//Press up arrow key to move forward on the Y AXIS
+			rigidb.AddForce (new Vector2 (-playerSpeed * Time.deltaTime, 0));
+		}
+		if (Input.GetKey ("right")) {//Press up arrow key to move forward on the Y AXIS
+			rigidb.AddForce (new Vector2 (playerSpeed * Time.deltaTime, 0));
+		}
+	}
+}
