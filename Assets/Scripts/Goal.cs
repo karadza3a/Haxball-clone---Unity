73cfebfa84090422f62b12fefea2688f212c4237
Andrej Karadzic <karadza3a@yahoo.com>
2015-11-24 21:35:40 +0100

    Added goal and goal scripts

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour
+{
+
+	public Player.Team team;
+	private BoxCollider2D goalBox;
+	private EdgeCollider2D goalLine;
+	private CircleCollider2D ball;
+
+	void Start ()
+	{
+		goalBox = GetComponent<BoxCollider2D> ();
+		goalLine = GetComponent<EdgeCollider2D> ();
+		ball = GameObject.FindGameObjectWithTag ("Ball").GetComponent<CircleCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (ball.IsTouching (goalBox) && !ball.IsTouching (goalLine)) {
+			Debug.Log (team);
+			ball.gameObject.transform.position = new Vector2 (0, 0);
+		}
+	}
+}
