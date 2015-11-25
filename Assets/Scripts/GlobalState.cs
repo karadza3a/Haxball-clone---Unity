d33086541a183fca90512b2b30b94a5e36342aea
Nikola Klipa <klipa.nikola@gmail.com>
2015-11-25 14:59:17 +0100

    Global state and Messager

---
+++ Assets/Scripts/GlobalState.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalState : MonoBehaviour {
+
+	public struct PlayerStruct{
+		public string name;
+		public Player.Team team;
+		
+		public PlayerStruct(string name, Player.Team team){
+			this.name = name;
+			this.team = team;
+		}
+	}
+
+	public static int homeScore;
+	public static int awayScore;
+	public static ArrayList homePlayers;
+	public static ArrayList awayPlayers;
+	public static float time;
+	public static Stack newPlayers = new Stack(); 
+
+	public static Player.Team getTeam(){
+		if (homePlayers.Count > awayPlayers.Count) {
+			return Player.Team.Away;
+		} else {
+			return Player.Team.Home;
+		}
+	}
+
+	public static int getPlayersCount(){
+		return homePlayers.Count + awayPlayers.Count;
+	}
+
+	public static string getScore(){
+		return homeScore.ToString() + ':' + awayScore.ToString();
+	}
+
+	void Update(){
+		time = time + Time.deltaTime;
+	}
+
+	void Start(){
+		time = 0.0f;
+		homePlayers = new ArrayList ();
+		awayPlayers = new ArrayList ();
+	}
+
+}
---
+++ Assets/Scripts/Server/Messager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class Messager : MonoBehaviour {
+	// login args = l;username;<EOM>
+	// play args  = p;username;number(bitmask);<EOM> 
+	// login back args = tim;velicinaterena;stative;<EOM>
+	// play back args = result;ball;allPlayers;<EOM>
+
+	public static void receiveMessage(string msg){
+		if (msg [0] == 'l') {
+			string[] words = msg.Split(';');
+			GlobalState.newPlayers.Push(new GlobalState.PlayerStruct(words[1], GlobalState.getTeam()));
+		} else {
+
+
+		}
+	}
+
+
+}
