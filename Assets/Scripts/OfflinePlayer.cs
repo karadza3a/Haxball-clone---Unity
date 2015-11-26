8a33ffb2bc4571023a46ba0f0d471bafbb4415d6
Andrej Karadzic <karadza3a@yahoo.com>
2015-11-26 17:19:47 +0100

    Offline player fine tuned
    Offline player fine tuned
    Reformatted in VisualStudio

--- Assets/Scripts/OfflinePlayer.cs
+++ Assets/Scripts/OfflinePlayer.cs
@@ -4,47 +4,54 @@ using System.Collections.Generic;
 
 public class OfflinePlayer : MonoBehaviour
 {
-	public string username;
-	public Pair[] keys;
+    public string username;
+    public Pair[] keys;
 
-	private Player player;
+    private Player player;
 
-	void Start ()
-	{
-		Messager.receiveMessage (string.Format ("l;{0}", username));
-	}
+    void Start()
+    {
+        Messager.receiveMessage(string.Format("l;{0}", username));
+    }
 
-	void FixedUpdate ()
-	{
-		if (player == null) {
-			foreach (Player pl in GlobalState.awayPlayers) {
-				if (pl.username == username) {
-					player = pl;
-					break;
-				}
-			}
-			foreach (Player pl in GlobalState.homePlayers) {
-				if (pl.username == username) {
-					player = pl;
-					break;
-				}
-			}
-			//not initialized yet - return
-			return;
-		}
-		Player.PressedKey pressed = 0;
-		foreach (Pair entry in keys) {
-			if (Input.GetKey ("" + entry.Key)) {
-				pressed |= entry.Value;
-			}
-		}
-		player.GetComponent<Player> ().SetPressedKeys (pressed);
-	}
-	
-	[System.Serializable]
-	public struct Pair
-	{
-		public char Key;
-		public Player.PressedKey Value;
-	}
+    void FixedUpdate()
+    {
+        if (player == null)
+        {
+            foreach (Player pl in GlobalState.awayPlayers)
+            {
+                if (pl.username == username)
+                {
+                    player = pl;
+                    break;
+                }
+            }
+            foreach (Player pl in GlobalState.homePlayers)
+            {
+                if (pl.username == username)
+                {
+                    player = pl;
+                    break;
+                }
+            }
+            //not initialized yet - return
+            return;
+        }
+        Player.PressedKey pressed = 0;
+        foreach (Pair entry in keys)
+        {
+            if (Input.GetKey("" + entry.Key))
+            {
+                pressed |= entry.Value;
+            }
+        }
+        player.GetComponent<Player>().SetPressedKeys(pressed);
+    }
+
+    [System.Serializable]
+    public struct Pair
+    {
+        public string Key;
+        public Player.PressedKey Value;
+    }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,58 +3,63 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float acceleration = 500;
-	public float shootDecceleration = .8f;
-	private Rigidbody2D rigidb;
-	private Player player;
+    public float acceleration = 500;
+    public float shootDecceleration = .8f;
+    private Rigidbody2D rigidb;
+    private Player player;
 
-	/**
-	 * Resets position, velocity and similar
-	 */
-	public void reset ()
-	{
-		Vector2 pos = new Vector2 (((int)player.team) * 5, 0);
-		transform.position = pos;
-		transform.rotation = Quaternion.identity;
-		rigidb.velocity = Vector2.zero;
-	}
+    /**
+     * Resets position, velocity and similar
+     */
+    public void reset()
+    {
+        Vector2 pos = new Vector2(((int)player.team) * Random.Range(5, 10), Random.Range(-10, 10));
+        transform.position = pos;
+        transform.rotation = Quaternion.identity;
+        rigidb.velocity = Vector2.zero;
+    }
 
-	void Start ()
-	{
-		rigidb = gameObject.GetComponent<Rigidbody2D> ();
-		player = gameObject.GetComponent<Player> ();
-	}
+    void Start()
+    {
+        rigidb = gameObject.GetComponent<Rigidbody2D>();
+        player = gameObject.GetComponent<Player>();
+    }
 
-	void FixedUpdate ()
-	{
-		Move ();
-	}
-	
-	void Move ()
-	{	
-		Vector2 v = new Vector2 (0, 0);
-		if (player.isPressed (Player.PressedKey.Up)) {
-			v += new Vector2 (0, 1);
-		}
-		if (player.isPressed (Player.PressedKey.Down)) {
-			v += new Vector2 (0, -1);
-		}
-		if (player.isPressed (Player.PressedKey.Left)) {
-			v += new Vector2 (-1, 0);
-		}
-		if (player.isPressed (Player.PressedKey.Right)) {
-			v += new Vector2 (1, 0);
-		}
-		AddLimitedForce (v);
-	}
+    void FixedUpdate()
+    {
+        Move();
+    }
 
-	void AddLimitedForce (Vector2 force)
-	{
-		force = force.normalized;
-		force = force * acceleration * Time.deltaTime;
-		if (player.isPressed (Player.PressedKey.Shoot)) {
-			force *= shootDecceleration;
-		}
-		rigidb.AddForce (force);
-	}
+    void Move()
+    {
+        Vector2 v = new Vector2(0, 0);
+        if (player.isPressed(Player.PressedKey.Up))
+        {
+            v += new Vector2(0, 1);
+        }
+        if (player.isPressed(Player.PressedKey.Down))
+        {
+            v += new Vector2(0, -1);
+        }
+        if (player.isPressed(Player.PressedKey.Left))
+        {
+            v += new Vector2(-1, 0);
+        }
+        if (player.isPressed(Player.PressedKey.Right))
+        {
+            v += new Vector2(1, 0);
+        }
+        AddLimitedForce(v);
+    }
+
+    void AddLimitedForce(Vector2 force)
+    {
+        force = force.normalized;
+        force = force * acceleration * Time.deltaTime;
+        if (player.isPressed(Player.PressedKey.Shoot))
+        {
+            force *= shootDecceleration;
+        }
+        rigidb.AddForce(force);
+    }
 }
