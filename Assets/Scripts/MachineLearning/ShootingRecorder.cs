749aa81124d1705154ddf9b2bf29d931b4b1469b
Andrej Karadzic <karadza3a@yahoo.com>
2015-12-10 01:41:45 +0100

    Worked on ShootingRecorder

--- Assets/Scripts/MachineLearning/ShootingRecorder.cs
+++ Assets/Scripts/MachineLearning/ShootingRecorder.cs
@@ -8,8 +8,9 @@ public class ShootingRecorder : MonoBehaviour
 {
 	public float power = 1300f;
 	
-	public int numberOfFramesToRecord = 200;
+	public int numberOfFramesToRecord = 10;
 	public int recordCounter = 0;
+	public int phase = 0;
 	public string outputPath = "/tmp/out.txt";
 		
 	private CircleCollider2D colider;
@@ -26,27 +27,48 @@ public class ShootingRecorder : MonoBehaviour
 		ballBody = ball.GetComponent<Rigidbody2D> ();
 		File.WriteAllText (outputPath, "");
 	}
-	
+
+	string strrr;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.C)) {
+			File.AppendAllText (outputPath, strrr);
+		}
+	}
+
 	void FixedUpdate ()
 	{
+		if (phase == 1) {
+			if (ballColider.IsTouchingLayers ()) {
+				recordCounter = numberOfFramesToRecord;
+				phase = 2;
+			}
+			return;
+		}
 		if (player.isPressed (Player.PressedKey.Shoot) 
 			&& colider.IsTouching (ballColider)) {
 			
 			recordCounter = numberOfFramesToRecord;
+			phase = 0;
 			
 			Vector2 bv = ballBody.velocity;
 			Vector2 bp = ballBody.gameObject.transform.position;
 			Vector2 pp = transform.position;
+			Vector2 pv = player.gameObject.GetComponent<Rigidbody2D> ().velocity;
 
-			File.AppendAllText (outputPath, "--," + pp.x.ToString () + "," + pp.y.ToString () + ","
-				+ bp.x.ToString () + "," + bp.y.ToString () + "," + bv.x.ToString () + "," + bv.y.ToString () + "," + "\n");
+			strrr = "--," + pp.x.ToString () + "," + pp.y.ToString () + ","
+				+ bp.x.ToString () + "," + bp.y.ToString () + "," + pv.x.ToString () + "," + pv.y.ToString () 
+				+ "," + bv.x.ToString () + "," + bv.y.ToString () + "," + "\n";
 		}
 		
 		if (recordCounter > 0) {
 			recordCounter --;
-
+			if (recordCounter == 0) {
+				phase++;
+			}
 			Vector3 pos = ballBody.gameObject.transform.position;
-			File.AppendAllText (outputPath, pos.x.ToString () + "," + pos.y.ToString () + "\n");
+			strrr += pos.x.ToString () + "," + pos.y.ToString () + "\n";
 
 		}
 	}
